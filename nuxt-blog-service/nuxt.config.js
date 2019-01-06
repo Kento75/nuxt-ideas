a0b07d155cbe71e7dde3fcb13748aa31fa6b04c6
Kento75 <kento2github@gmail.com>
2019-01-06 10:34:21 +0900

    Fix nuxt.config.js adding axios baseURL for firebase RealtimeDatabase

--- nuxt-blog-service/nuxt.config.js
+++ nuxt-blog-service/nuxt.config.js
@@ -49,6 +49,7 @@ module.exports = {
   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
+    baseURL: 'https://nuxt-blog-service-kento75.firebaseio.com'
   },
 
   /*
