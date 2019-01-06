0cc688c9254005a9c21e5dd5a97cd51c0ba8f8fa
Kento75 <kento2github@gmail.com>
2019-01-06 18:06:34 +0900

    Add post publish logic

--- nuxt-blog-service/app/store/index.js
+++ nuxt-blog-service/app/store/index.js
@@ -18,7 +18,7 @@ export const mutations = {
 export const actions = {
   // ログインアクション
   async login({ commit }, { id }) {
-    const user = await this.$axios.$get(`/usres/${id}.json`)
+    const user = await this.$axios.$get(`/users/${id}.json`)
     
     if(!user.id) throw new Error('Invalid user')
     commit('setUser', { user })
@@ -27,7 +27,8 @@ export const actions = {
   async register({ commit }, { id }) {
     const payload = {}
     payload[id] = { id }
-    const user = await this.$axios.$patch(`/users/${id}.json`)
+    await this.$axios.$patch(`/users.json`, payload)
+    const user = await this.$axios.$get(`/users/${id}.json`)
 
     if(!user.id) throw new Error('Invalid user')
     commit('setUser', { user })
---
+++ nuxt-blog-service/app/store/posts.js
@@ -0,0 +1,46 @@
+import moment from '~/plugins/moment'
+
+export const state = () => ({
+  posts: []
+})
+
+export const getters = {
+  posts: (state) => state.posts
+}
+
+export const mutations = {
+  // 記事追加
+  addPost(state, { post }) {
+    state.posts.push(post)
+  },
+  // 記事更新
+  updatePost(state, { post }) {
+    // 対象の記事を編集後の記事に置き換える
+    state.posts = state.posts.map((p) => (p.id === post.id ? post : p))
+  },
+  clearPosts(state) {
+    state.posts = []
+  }
+}
+
+export const actions = {
+  async publishPost({ commit }, { payload }) {
+    const user = await this.$axios.$get(`/users/${payload.user.id}.json`)
+    const post_id = (await this.$axios.$post(`/posts.json`, payload)).name
+    const created_at = moment().format()
+
+    // 記事データ作成
+    const post = { id: post_id, ...payload, created_at }
+    const putData = { id: post_id, ...payload, created_at }
+
+    // ユーザーデータは必要ないため削除
+    delete putData.user
+
+    // 記事データを登録
+    await this.$axios.$put(`/users/${user.id}/posts.json`, [
+      ...(user.posts || []),
+      putData
+    ])
+    commit('addPost', { post })
+  }
+}
